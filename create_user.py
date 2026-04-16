728eb19d3a1cc8d99b7b6d65b8e5c73cff18125f
Arayz-Wasti <arayzwasti111@gmail.com>
2026-04-16 19:53:21 +0500

    Added create user.

--- create_user.py
+++ create_user.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")  # change if your project name differs
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
@@ -10,15 +10,17 @@ User = get_user_model()
 
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+
+print("EMAIL:", email)
 
 if email and password:
     if not User.objects.filter(email=email).exists():
         User.objects.create_superuser(
-            username=username,
             email=email,
             password=password
         )
-        print("Superuser created")
+        print("Superuser created successfully")
     else:
         print("Superuser already exists")
+else:
+    print("Missing env variables")
